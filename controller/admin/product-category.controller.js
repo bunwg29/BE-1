cf97a64bddc5d6631b96f8926c17e4e5c990b3f3
Bun <bun2932005@gmail.com>
2024-10-09 21:02:00 +0700

    feat: category at admin

--- controller/admin/product-category.controller.js
+++ controller/admin/product-category.controller.js
@@ -1,6 +1,9 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
 
+const createTreeHelper = require("../../helpers/createTree");
+
+// [GET] /admin/product-category
 // [GET] /admin/product-category
 module.exports.index = async (req, res) => {
     let find = {
@@ -9,34 +12,54 @@ module.exports.index = async (req, res) => {
   
     const records = await ProductCategory.find(find);
   
-   
+    const newRecords = createTreeHelper.tree(records);
   
     res.render("admin/pages/products-category/index", {
-        pageTitle: "Danh mục sản phẩm",
-        records: records
+      pageTitle: "Danh mục sản phẩm",
+      records: newRecords 
     });
   };
-
-module.exports.create = (req, res) => {
-
-    res.render("admin/pages/products-category/create", {
-        pageTitle: "Tạo danh mục",
-    });
-
-};
-
-module.exports.createPost = async (req, res) => {
   
-    if (req.body.position == "") {
-        const count = await ProductCategory.countDocuments();
-        req.body.position = count + 1;
-    } else {
-        req.body.position = parseInt(req.body.position);
+  // [GET] /admin/product-category/create
+  module.exports.create = async (req, res) => {
+    let find = {
+      deleted: false
     }
-
-    const record = new ProductCategory(req.body);
-    await record .save(); 
-
-    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
-
-};
+  
+    const records = await ProductCategory.find(find);
+    
+    const newRecords = createTreeHelper.tree(records);
+  
+    console.log(newRecords);
+      
+    res.render("admin/pages/products-category/create", {
+      pageTitle: "Tạo danh mục sản phẩm",
+      records: newRecords
+    });
+  }
+  
+  
+  // [POST] /admin/products/createPost
+  module.exports.createPost = async (req, res) => {
+    const permissions = res.locals.role.permissions;
+  
+    if(permissions.includes("products-category_create")) {
+      if(req.body.position == "") {
+        const count= await ProductCategory.count();
+        req.body.position = count + 1;
+      } else {
+        req.body.position = parseInt(req.body.position);
+      } 
+      const record = new ProductCategory(req.body);
+      await record .save(); 
+    
+      res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  
+    } else {
+      res.send("403");
+      return;  
+    }
+    
+    
+  
+  }
--- models/product-category.model.js
+++ models/product-category.model.js
@@ -1,14 +1,14 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const slug = require("mongoose-slug-updater");
 
-mongoose.plugin(slug);
 
+mongoose.plugin(slug)
 const productCategorySchema = new mongoose.Schema(
     {
-        title: String,
-        parent_id : {
+        title: String,   
+        parent_id: {
             type: String,
-            default: ""
+            default: "",
         },
         description: String,
         thumbnail: String,
@@ -22,14 +22,16 @@ const productCategorySchema = new mongoose.Schema(
         deleted: {
             type: Boolean,
             default: false
-        },
+        }, 
         deletedAt: Date
-    },
+
+    }, 
     {
-        timestamps: true
+        timestamps: true    
     }
 );
 
-const ProductCategory = mongoose.model('ProductCategory', productCategorySchema, "products-category");
 
-module.exports = ProductCategory;
+const ProductCategory = mongoose.model("ProductCategory", productCategorySchema, "products-category");
+
+module.exports = ProductCategory; 
